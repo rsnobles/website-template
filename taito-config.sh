fdc7fb5df2e0680cfaa955667bb45721e1a746ab
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-03-31 00:12:15 +0200

    feat: cypress bug workaround

--- taito-config.sh
+++ taito-config.sh
@@ -215,3 +215,10 @@ if [[ "$taito_target_env" == "local" ]]; then
 else
   CYPRESS_baseUrl=$ci_test_base_url
 fi
+
+# Hack to avoid basic auth on Electron browser startup:
+# https://github.com/cypress-io/cypress/issues/1639
+CYPRESS_baseUrlHack=$CYPRESS_baseUrl
+CYPRESS_baseUrl=https://www.google.com
+test_www_CYPRESS_baseUrlHack=$test_client_CYPRESS_baseUrl
+test_www_CYPRESS_baseUrl=https://www.google.com
--- www/test/integration/site.spec.js
+++ www/test/integration/site.spec.js
@@ -1,9 +1,13 @@
 // / <reference types="Cypress" />
 
+// Hack to avoid basic auth on Electron browser startup:
+// https://github.com/cypress-io/cypress/issues/1639
+const url = Cypress.env('baseUrlHack');
+
 describe('Site', () => {
   beforeEach(() => {});
 
   it('Shows the site', () => {
-    cy.visit('/');
+    cy.visit(`${url}/`);
   });
 });
