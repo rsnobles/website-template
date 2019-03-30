191df330db6e7bf5cb78e928d2b29a321ec2410e
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-03-30 17:44:54 +0200

    chore: latest changes from server-template

--- scripts/taito-template/upgrade.sh
+++ scripts/taito-template/upgrade.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 : "${template_project_path:?}"
 
@@ -6,30 +6,27 @@ echo
 ./scripts/taito-template/init.sh
 
 # Copy files from template to project
-cp README.md "${template_project_path}"
-cp cloudbuild.yaml "${template_project_path}"
-mkdir -p "${template_project_path}/scripts"
+yes | cp CONFIGURATION.md "${template_project_path}"
+yes | cp DEVELOPMENT.md "${template_project_path}"
+yes | cp cloudbuild.yaml "${template_project_path}"
 
-cp "${template_project_path}/scripts/helm/.htpasswd" \
-  "${template_project_path}/scripts/.htpasswd" 2> /dev/null
-rm -rf "${template_project_path}/scripts/helm" 2> /dev/null
-cp -r "scripts/helm" "${template_project_path}/scripts/helm" 2> /dev/null
-rm -f "${template_project_path}/scripts/helm/.htpasswd" 2> /dev/null
-mv "${template_project_path}/scripts/.htpasswd" \
-  "${template_project_path}/scripts/helm/.htpasswd" 2> /dev/null
+mkdir -p "${template_project_path}/scripts/helm"
+yes | cp -r "scripts/helm/requirements.yaml" \
+  "${template_project_path}/scripts/helm/requirements.yaml" 2> /dev/null
+yes | cp "www/*" \
+  "${template_project_path}/www/" 2> /dev/null
 
 echo
 echo
 echo "--- Manual steps ---"
 echo
-echo "Do the following:"
-echo "- Remove extra images from the beginning of the cloudbuild.yaml file"
-echo "- Generate links to README.md by running: taito project docs"
-echo "- Add project specific modifications back to README.md if there were any"
-echo "  (README.md was copied from the template)"
+echo "Recommended steps:"
+echo "- Update libraries and other project dependencies."
 echo
 echo "If something stops working, try the following:"
 echo "- Run 'taito --upgrade' to upgrade your taito-cli"
+echo "- Compare scripts/helm*.yaml with the template"
 echo "- Compare taito-config.sh with the template"
 echo "- Compare package.json with the template"
+echo "- Compare cloudbuild.yaml with the previous version"
 echo
