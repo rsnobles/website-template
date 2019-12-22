c97af5d67bff69056f3d7faaa6815ebad80bb79d
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-12-23 00:58:49 +0200

    feat(test): read basic auth from secrets, remove baseUrlHack

--- taito-testing-config.sh
+++ taito-testing-config.sh
@@ -9,20 +9,15 @@
 ##########################################################################
 
 # Environment specific settings
-case $taito_env in
-  local)
-    # local environment
-    ci_test_base_url=http://website-template-ingress:80
-    ;;
-  *)
-    # dev and feature environments
-    if [[ $taito_env == "dev" ]] || [[ $taito_env == "f-"* ]]; then
-      ci_exec_test=false        # enable this to execute test suites
-      ci_exec_test_init=false   # run 'init --clean' before each test suite
-      ci_test_base_url=https://username:secretpassword@$taito_domain
-    fi
-    ;;
-esac
+if [[ $taito_env == "local" ]]; then
+  ci_test_base_url=http://website-template-ingress:80
+elif [[ $taito_env == "dev" ]] || [[ $taito_env == "f-"* ]]; then
+  ci_exec_test=false        # enable this to execute test suites
+  ci_exec_test_init=false   # run 'init --clean' before each test suite
+  if [[ $taito_command == "util-test" ]]; then
+    ci_test_base_url=https://$(taito -q secret show:$taito_env basic-auth | head -1)@$taito_domain
+  fi
+fi
 
 # URLs
 test_www_CYPRESS_baseUrl=$ci_test_base_url
@@ -31,10 +26,3 @@ if [[ "$taito_target_env" == "local" ]]; then
 else
   CYPRESS_baseUrl=$ci_test_base_url
 fi
-
-# Hack to avoid basic auth on Electron browser startup:
-# https://github.com/cypress-io/cypress/issues/1639
-CYPRESS_baseUrlHack=$CYPRESS_baseUrl
-CYPRESS_baseUrl=https://www.google.com
-test_www_CYPRESS_baseUrlHack=$test_client_CYPRESS_baseUrl
-test_www_CYPRESS_baseUrl=https://www.google.com
--- www/test/integration/site.spec.js
+++ www/test/integration/site.spec.js
@@ -1,13 +1,9 @@
 // / <reference types="Cypress" />
 
-// Hack to avoid basic auth on Electron browser startup:
-// https://github.com/cypress-io/cypress/issues/1639
-const url = Cypress.env('baseUrlHack');
-
 describe('Site', () => {
   beforeEach(() => {});
 
   it('Shows the site', () => {
-    cy.visit(`${url}/`);
+    cy.visit('/');
   });
 });
